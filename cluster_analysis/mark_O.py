f6853ded56968a5bb386bf9a4772a5bff3024ca7
IvanPatrenkov <ivanestver24@gmail.com>
2025-04-12 22:10:17 +0300

    add: Оптимизировал алгоритм выделения кристаллитов, сократив время выполнения

--- cluster_analysis/mark_O.py
+++ cluster_analysis/mark_O.py
@@ -43,7 +43,7 @@ def do_traverse(main_direction: Side, directions: list[Side], curr_monomer: Mono
     else:
         pot_cluster.clear()
 
-def find_clusters(current_globula: GlobulaView, axis: Axis):
+def find_clusters(current_globula: GlobulaView, axis: Axis, avg_tuple: tuple[int, Axis] = None):
     clusters = list[Cluster]()
     def fill_clusters(directions, main_direction):
         pot_cluster: list[Monomer] = [monomer, monomer.sides[main_direction]]
@@ -56,6 +56,7 @@ def find_clusters(current_globula: GlobulaView, axis: Axis):
             if main_direction == Side.Undefined:
                 continue
 
+            old_size = len(clusters)
             if axis == Axis.X_AXIS and main_direction in [Side.Forward, Side.Backward]:
                 fill_clusters([Side.Left, Side.Down, Side.Right], main_direction)
                 fill_clusters([Side.Left, Side.Up, Side.Right], main_direction)
@@ -74,7 +75,14 @@ def find_clusters(current_globula: GlobulaView, axis: Axis):
             else:
                 continue
 
-    return clusters
+            if old_size != len(clusters):
+                clusters = gather_clusters(clusters, avg_tuple, old_size)
+
+    # Although we seem to have all the clusters already joined
+    # it is possible that some clusters can be joined.
+    # Since here we don't have a lot of them, this step shouldn't be
+    # too time-consuming
+    return gather_clusters(clusters, avg_tuple, None)
 
 def get_monomers_count_in_clusters(clusters: list[Cluster]):
     s = set[Monomer]()
@@ -83,12 +91,18 @@ def get_monomers_count_in_clusters(clusters: list[Cluster]):
 
     return len(s)
 
-def gather_clusters(clusters: list[Cluster], avg_tuple : tuple[int, Axis] = None):
-    if avg_tuple is not None:
-        avg, avg_axis = avg_tuple
-        while True:
-            clusters_new = list[Cluster]()
-            d = dict[int, list[int]]()
+def gather_clusters(clusters: list[Cluster], avg_tuple : tuple[int, Axis] = None, start_from: int = None):
+    if avg_tuple is None:
+        return clusters
+
+    avg, avg_axis = avg_tuple
+    while True:
+        clusters_new = list[Cluster]()
+        # Build a dict where for each i we map a list of js that can be joined with i
+        d = dict[int, list[int]]()
+        # If start_from is None, then we need to compare all the cluster between each other
+        # to do the deep check of clusters possible to join
+        if start_from is None:
             for i in range(len(clusters) - 1):
                 for j in range(i + 1, len(clusters)):
                     joined_cluster = join_clusters(clusters[i], clusters[j])
@@ -97,56 +111,80 @@ def gather_clusters(clusters: list[Cluster], avg_tuple : tuple[int, Axis] = None
                             d[i] = list[int]()
                             
                         d[i].append(j)
+        # The algorithm of highlighing clusters determine that all the new clusters are added
+        # into the end of the clusters list, therefore, it is assumed it isn't able to join clusters
+        # before start_from. So we need to check only the new ones
+        else:
+            for i in range(start_from, len(clusters)):
+                for j in range(start_from):
+                    joined_cluster = join_clusters(clusters[i], clusters[j])
+                    if joined_cluster is not None:
+                        if i not in d.keys():
+                            d[i] = list[int]()
+                            
+                        d[i].append(j)
 
-            if len(d) == 0:
-                break
+        # If there's nothing to join, stop the algorithm
+        if len(d) == 0:
+            break
 
-            def join_joined_clusters(current_node: int):
-                joined_clusters_for_current_node = list[int]()
-                if current_node not in d.keys() or len(d[current_node]) == 0:
-                    return joined_clusters_for_current_node
-                
-                for value in d[current_node]:
-                    joined_clusters_for_current_node.append(value)
-                    received = join_joined_clusters(value)
-                    for rec_value in received:
-                        if rec_value not in joined_clusters_for_current_node:
-                            joined_clusters_for_current_node.append(rec_value)
-                    
-                d[current_node].clear()
+        # Now join the extracted clusters recursively
+        # that means that if we, e.g, joined clusters 1 and 2 into a 1-2 cluster
+        # and cluster 2 and 3 into a 2-3 cluster, therefore, the joined ones have a common set of monomers
+        # which are in the cluster 2 and can be joined as well into a 1-2-3 cluster
+        def join_joined_clusters(current_node: int):
+            joined_clusters_for_current_node = list[int]()
+            # If nothing to join, come back
+            if current_node not in d.keys() or len(d[current_node]) == 0:
                 return joined_clusters_for_current_node
             
-            for key in d.keys():
-                d[key] = join_joined_clusters(key) 
-            
-            joined_clusters_indexes = set()
-            for key in d.keys():
-                joined_clusters_indexes.add(key)
-                joined_clusters_indexes.update(d[key])
+            for value in d[current_node]:
+                # Add the current cluster as the connection
+                joined_clusters_for_current_node.append(value)
+                # Recursively find others ready to be joined
+                received = join_joined_clusters(value)
+                for rec_value in received:
+                    if rec_value not in joined_clusters_for_current_node:
+                        joined_clusters_for_current_node.append(rec_value)
+                
+            # All clusters for this cluster are to become a part of another list,
+            # therefore, clear the current to avoid issues
+            d[current_node].clear()
+            return joined_clusters_for_current_node
+        
+        # Join the clusters. In fact, find the numbers belonging to the same cluster
+        for key in d.keys():
+            d[key] = join_joined_clusters(key) 
+        
+        # Do actual join
+        joined_clusters_indexes = set()
+        for key in d.keys():
+            joined_clusters_indexes.add(key)
+            joined_clusters_indexes.update(d[key])
 
-                if len(d[key]) == 0:
+            if len(d[key]) == 0:
+                continue
+
+            curr_cluster = clusters[key]
+            for value in d[key]:
+                c = join_clusters(curr_cluster, clusters[value])
+                if c is None:
                     continue
+                curr_cluster = c
+            clusters_new.append(curr_cluster)
 
-                curr_cluster = clusters[key]
-                for value in d[key]:
-                    c = join_clusters(curr_cluster, clusters[value])
-                    if c is None:
-                        continue
-                    curr_cluster = c
-                clusters_new.append(curr_cluster)
+        # Those clusters not been touched must be moved as well
+        indexes_not_joined = set([i for i in range(len(clusters)) if i not in joined_clusters_indexes])
+        for i in indexes_not_joined:
+            clusters_new.append(clusters[i])
+        
+        clusters = clusters_new
 
-            indexes_not_joined = set([i for i in range(len(clusters)) if i not in joined_clusters_indexes])
-            for i in indexes_not_joined:
-                clusters_new.append(clusters[i])
-            
-            clusters = clusters_new
-
-        clusters = set([c for c in clusters if c.get_avg_length_by_axis(avg_axis) >= avg])
-    return clusters
+    # return those which satisfy the condition
+    return list(set([c for c in clusters if c.get_avg_length_by_axis(avg_axis) >= avg]))
 
 def mark_clusters(current_globula: GlobulaView, axis: Axis, avg_tuple: tuple[int, Axis] = None):
-    clusters = find_clusters(current_globula, axis)
-    clusters = join_clusters(clusters, avg_tuple)
+    clusters = find_clusters(current_globula, axis, avg_tuple)
             
     axis_type_dict = {
         Axis.X_AXIS: MonomerType.Owise,
@@ -157,9 +195,9 @@ def mark_clusters(current_globula: GlobulaView, axis: Axis, avg_tuple: tuple[int
         cluster.set_type_of_monomers(axis_type_dict[axis])
 
 def mark_common_clusters(current_globula: GlobulaView):
-    clusters_X = gather_clusters(find_clusters(current_globula, Axis.X_AXIS), (0, Axis.X_AXIS))
-    clusters_Y = gather_clusters(find_clusters(current_globula, Axis.Y_AXIS), (0, Axis.Y_AXIS))
-    clusters_Z = gather_clusters(find_clusters(current_globula, Axis.Z_AXIS), (0, Axis.Z_AXIS))
+    clusters_X = set(find_clusters(current_globula, Axis.X_AXIS, (0, Axis.X_AXIS)))
+    clusters_Y = set(find_clusters(current_globula, Axis.Y_AXIS, (0, Axis.Y_AXIS)))
+    clusters_Z = set(find_clusters(current_globula, Axis.Z_AXIS, (0, Axis.Z_AXIS)))
 
     clusters_X_Y = list([intersect_clusters(cluster_X, cluster_Y) for cluster_X in clusters_X for cluster_Y in clusters_Y])
     clusters_X_Y = list([mon for cluster in clusters_X_Y for mon in cluster])
@@ -207,9 +245,9 @@ def mark_common_clusters(current_globula: GlobulaView):
                 break
 
 def mark_biggest_clusters(current_globula: GlobulaView):
-    clusters_X = gather_clusters(find_clusters(current_globula, Axis.X_AXIS), (0, Axis.X_AXIS))
-    clusters_Y = gather_clusters(find_clusters(current_globula, Axis.Y_AXIS), (0, Axis.Y_AXIS))
-    clusters_Z = gather_clusters(find_clusters(current_globula, Axis.Z_AXIS), (0, Axis.Z_AXIS))
+    clusters_X = find_clusters(current_globula, Axis.X_AXIS, (0, Axis.X_AXIS))
+    clusters_Y = find_clusters(current_globula, Axis.Y_AXIS, (0, Axis.Y_AXIS))
+    clusters_Z = find_clusters(current_globula, Axis.Z_AXIS, (0, Axis.Z_AXIS))
     total_monomers_count = sum([pol.len() for pol in current_globula])
 
     clusters_X = sorted(clusters_X, key=lambda c1: c1.size, reverse=True)
