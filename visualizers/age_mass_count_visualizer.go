4e32306dca10bb54cdc246562fd2837353c4f697
IvanPatrenkov <ivanestver24@gmail.com>
2025-07-02 23:07:00 +0300

    add: Добавление визуализатора

---
+++ visualizers/age_mass_count_visualizer.go
@@ -0,0 +1,10 @@
+package visualizers
+
+import dt "data_visualizer/datatypes"
+
+type sAgeMassCountVisualizer struct {
+}
+
+func (visualizer *sAgeMassCountVisualizer) Visualize(atoms []dt.Atom, bonds dt.Bonds) {
+
+}
---
+++ visualizers/visualizer_interface.go
@@ -0,0 +1,38 @@
+package visualizers
+
+import (
+	dt "data_visualizer/datatypes"
+	"strconv"
+)
+
+const (
+	VISUALIZER_TYPE_UNKNOWN    = -1
+	VISUALIZER_TYPE_MASS_COUNT = iota
+	VISUALIER_TYPE_COUNT
+)
+
+type VisualizerType = int
+
+var visualizerTypesStrings map[VisualizerType]string = map[VisualizerType]string{
+	VISUALIZER_TYPE_MASS_COUNT: "Parts Masses-Count Distribution after Aging",
+}
+
+func GetAllVisualizersDescription() []string {
+	description := make([]string, VISUALIER_TYPE_COUNT-VISUALIZER_TYPE_MASS_COUNT)
+	for t, s := range visualizerTypesStrings {
+		description = append(description, strconv.Itoa(t)+" - "+s)
+	}
+	return description
+}
+
+type IVisualizer interface {
+	Visualize(atoms []dt.Atom, bonds dt.Bonds)
+}
+
+func NewVisualizer(visualizerType VisualizerType) IVisualizer {
+	switch visualizerType {
+	case VISUALIZER_TYPE_MASS_COUNT:
+		return new(sAgeMassCountVisualizer)
+	}
+	return nil
+}
