fe6a6e79663786ac8cd156fdc81231d95f2e985b
IvanPatrenkov <ivanestver24@gmail.com>
2025-10-12 23:42:52 +0300

    add: add logging and requiring of some parameters

--- visualizers/age_mass_count_visualizer.go
+++ visualizers/age_mass_count_visualizer.go
@@ -2,10 +2,12 @@ package visualizers
 
 import (
 	dt "data_visualizer/datatypes"
+	"fmt"
 	"math"
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
@@ -43,8 +45,6 @@ func (s *sStack) isNotEmpty() bool {
 	return len(s.s) != 0
 }
 
-const STEPS_COUNT = 1000
-
 type sAgeMassCountVisualizer struct {
 	atoms *[]dt.Atom
 	bonds *dt.Bonds
@@ -75,6 +75,7 @@ func (visualizer *sAgeMassCountVisualizer) dst() []int {
 	}
 
 	masses := make([]int, 0)
+	forbiddenAtomTypes := visualizer.getForbiddenAtomTypes()
 	// Do until all atoms have been visited
 	for !allIsTrue(&usedAtoms) {
 		// Firstly, we need to find a point to start from
@@ -89,17 +90,48 @@ func (visualizer *sAgeMassCountVisualizer) dst() []int {
 		stack.push(startPoint)
 		for stack.isNotEmpty() {
 			currentPoint := stack.pop()
-			visitedPointsCount++
 			usedAtoms[currentPoint] = true
+			if isForbidden(&forbiddenAtomTypes, (*visualizer.atoms)[currentPoint].M_AtomType) {
+				break
+			}
+			visitedPointsCount++
 			for _, nextPoint := range (*visualizer.bonds)[currentPoint] {
 				if !usedAtoms[nextPoint] {
 					stack.push(nextPoint)
 				}
 			}
 		}
-		masses = append(masses, visitedPointsCount)
+		if visitedPointsCount != 0 {
+			masses = append(masses, visitedPointsCount)
+		}
 	}
 	return masses
+}
+
+func (visualizer *sAgeMassCountVisualizer) getForbiddenAtomTypes() []int {
+	fmt.Println("Please, specify the forbidden atom types. They won't be considered in analysis. Note that they MUST be enumerated as numbers separated by one space")
+	var s string
+	fmt.Scanln(&s)
+	numbers := strings.Split(s, " ")
+	var forbiddenAtomTypes []int
+	for _, n := range numbers {
+		i, err := strconv.Atoi(n)
+		if err != nil {
+			fmt.Errorf("%s is not number", n)
+			return []int{}
+		}
+		forbiddenAtomTypes = append(forbiddenAtomTypes, i)
+	}
+	return forbiddenAtomTypes
+}
+
+func isForbidden(forbiddenAtomTypes *[]int, sample int) bool {
+	for _, i := range *forbiddenAtomTypes {
+		if i == sample {
+			return true
+		}
+	}
+	return false
 }
 
 func (visualizer *sAgeMassCountVisualizer) makeDistributions(masses []int) map[int]int {
@@ -113,7 +145,11 @@ func (visualizer *sAgeMassCountVisualizer) makeDistributions(masses []int) map[i
 			maxMass = mass
 		}
 	}
-	step := int(math.Ceil(float64(len(*visualizer.atoms)) / STEPS_COUNT))
+	range_ := maxMass - minMass
+	fmt.Println("The range of masses is " + strconv.Itoa(range_) + ". What divider to use?")
+	STEPS_COUNT := 100
+	fmt.Scanln(&STEPS_COUNT)
+	step := int(math.Ceil(float64(range_) / float64(STEPS_COUNT)))
 	distribution := make(map[int]int)
 	if step == 0 {
 		return distribution
@@ -161,6 +197,9 @@ func (visualizer *sAgeMassCountVisualizer) showMasses(masses []int, outputName s
 	bar := charts.NewBar()
 	xValues, yValues := getXYValues(&distribution)
 	bar.SetXAxis(xValues).AddSeries("Placeholder", yValues)
-	f, _ := os.Create(outputName + ".html")
+	outputName += ".html"
+	fmt.Println("Save into the file " + outputName)
+	f, _ := os.Create(outputName)
 	bar.Render(f)
+	defer f.Close()
 }
