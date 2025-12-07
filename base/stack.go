31549bd1e5e5e43a311d02d2b18c4859febbce45
IvanPatrenkov <ivanestver24@gmail.com>
2025-12-08 00:05:39 +0300

    add: simple stack realization

---
+++ base/stack.go
@@ -0,0 +1,29 @@
+package base
+
+type Stack []interface{}
+
+func (s *Stack) Push(item interface{}) {
+	*s = append(*s, item)
+}
+
+func (s *Stack) Pop() (interface{}, bool) {
+	if s.IsEmpty() {
+		return nil, false // Or handle error appropriately
+	}
+	index := len(*s) - 1
+	element := (*s)[index]
+	*s = (*s)[:index]
+	return element, true
+}
+
+func (s *Stack) Peek() (interface{}, bool) {
+	if s.IsEmpty() {
+		return nil, false // Or handle error appropriately
+	}
+	index := len(*s) - 1
+	return (*s)[index], true
+}
+
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
