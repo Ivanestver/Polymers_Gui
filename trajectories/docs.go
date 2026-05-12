e15040a8b4d7dd441130d05d95a842587d0ef1c6
Ivan Patrenkov <ivanestver24@gmail.com>
2026-05-12 23:35:37 +0300

    add: trajectories module in its initial state

---
+++ trajectories/docs.go
@@ -0,0 +1,4 @@
+/*
+Package trajectories is a module that applies trajectories to a globula and saves every step.
+*/
+package trajectories
---
+++ trajectories/trajectories.go
@@ -0,0 +1,10 @@
+package trajectories
+
+import (
+	"errors"
+	"polymers/views"
+)
+
+func ApplyTrajectories(globula *views.GlobulaView, trajectoriesFilename string) error {
+	return errors.New("не реализовано")
+}
