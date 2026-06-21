8614af79e425c7c0ba2d586f5761695d86c82b0f
Ivan Patrenkov <ivanestver24@gmail.com>
2026-06-21 11:59:35 +0300

    add: old trajectory alg is postponed bu the new one takes multiple trajectory files

--- trajectories/trajectories.go
+++ trajectories/trajectories.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"polymers/base"
+	"polymers/cristallinity"
 	"polymers/datatypes"
 	"polymers/savers"
 	"polymers/views"
@@ -14,22 +15,18 @@ import (
 )
 
 type _TrajectoriesApplier struct {
-	globula              *views.GlobulaView
-	trajectoriesFileName string
-	fileReader           *bufio.Scanner
-	monomers             map[int64]*datatypes.Monomer
+	globula               *views.GlobulaView
+	trajectoriesFileNames []string
+	fileReader            *bufio.Scanner
+	monomers              map[int64]*datatypes.Monomer
+	currentTimestep       int
 }
 
-func newTrajectoriesApplier(globula *views.GlobulaView, trajectoriesFilename string) (*_TrajectoriesApplier, error) {
+func newTrajectoriesApplier(globula *views.GlobulaView, trajectoriesFilenames []string) (*_TrajectoriesApplier, error) {
 	applier := &_TrajectoriesApplier{
-		globula:              globula,
-		trajectoriesFileName: trajectoriesFilename,
+		globula:               globula,
+		trajectoriesFileNames: trajectoriesFilenames,
 	}
-	file, err := os.Open(trajectoriesFilename)
-	if err != nil {
-		return nil, err
-	}
-	applier.fileReader = bufio.NewScanner(file)
 	applier.monomers = make(map[int64]*datatypes.Monomer)
 	for polNumber := 0; polNumber < globula.Len(); polNumber++ {
 		polymer := globula.GetPolymerByIdx(polNumber)
@@ -38,65 +35,64 @@ func newTrajectoriesApplier(globula *views.GlobulaView, trajectoriesFilename str
 			applier.monomers[monomer.Number] = monomer
 		}
 	}
+	applier.currentTimestep = 0
 	return applier, nil
 }
 
 func (applier *_TrajectoriesApplier) apply() error {
-	for {
-		timestep, err := applier.getNextTimestep()
+	fileForS, err := os.Create("s.csv")
+	if err != nil {
+		return err
+	}
+	defer fileForS.Close()
+	fileForS.WriteString("timestamp;SMeanCos;STensor\n")
+	for _, filename := range applier.trajectoriesFileNames {
+		file, err := os.Open(filename)
 		if err != nil {
 			return err
-		} else if timestep == -1 {
-			break
 		}
-		fmt.Printf("Timestep: %d\n", timestep)
+		applier.fileReader = bufio.NewScanner(file)
 		numberOfAtoms, err := applier.getNumberOfAtoms()
 		if err != nil {
 			return err
 		}
-		box, err := applier.getBoxBounds()
-		if err != nil {
-			return err
-		}
-		// Apply trajectories
-		if err = applier.makeStep(numberOfAtoms, box); err != nil {
-			return err
-		}
-		// Save results
-		if err := applier.saveTimestep(timestep); err != nil {
-			return err
+		for {
+			timestep := applier.getNextTimestep()
+			fmt.Printf("Timestep: %d\n", timestep)
+			// Apply trajectories
+			if err = applier.makeStep(numberOfAtoms); err != nil {
+				break
+			}
+			if err := applier.calculateS(timestep, fileForS); err != nil {
+				return err
+			}
+			// Save results
+			if err := applier.saveTimestep(timestep); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
 }
 
-func (applier *_TrajectoriesApplier) getNextTimestep() (int, error) {
-	for applier.fileReader.Scan() && !strings.Contains(applier.fileReader.Text(), "TIMESTEP") {
-	}
-	if applier.fileReader.Scan() {
-		line := applier.fileReader.Text()
-		line = strings.Trim(line, " ")
-		timestep, err := strconv.Atoi(line)
-		if err == nil {
-			return timestep, nil
-		} else {
-			return -1, err
-		}
-	}
-	return -1, nil
+func (applier *_TrajectoriesApplier) getNextTimestep() int {
+	applier.currentTimestep++
+	return applier.currentTimestep
 }
 
 func (applier *_TrajectoriesApplier) getNumberOfAtoms() (int, error) {
-	for applier.fileReader.Scan() && !strings.Contains(applier.fileReader.Text(), "NUMBER OF ATOMS") {
-	}
-	if applier.fileReader.Scan() {
-		line := applier.fileReader.Text()
-		line = strings.Trim(line, " ")
-		numberOfAtoms, err := strconv.Atoi(line)
-		if err == nil {
-			return numberOfAtoms, nil
-		} else {
-			return -1, err
+	for applier.fileReader.Scan() {
+		if strings.Contains(applier.fileReader.Text(), "TotAtms") {
+			line := applier.fileReader.Text()
+			line = strings.Trim(line, " ")
+			line = base.ReduceSpacesToSingle(line)
+			parts := strings.Split(line, " ")
+			numberOfAtoms, err := strconv.Atoi(parts[4])
+			if err == nil {
+				return numberOfAtoms, nil
+			} else {
+				return -1, err
+			}
 		}
 	}
 	return -1, errors.New("больше нет секций NUMBER OF ATOMS")
@@ -148,22 +144,65 @@ func (applier *_TrajectoriesApplier) calculateNewMonomerCoords(currCoords base.V
 	return newCoords
 }
 
-func (applier *_TrajectoriesApplier) makeStep(numberOfAtoms int, box _BoxBounds) error {
-	field := applier.globula.GetPolymerByIdx(0).GetUnderlinedField()
-	for applier.fileReader.Scan() && !strings.Contains(applier.fileReader.Text(), "id type") {
-	}
-	for i := 0; i < numberOfAtoms; i++ {
-		if !applier.fileReader.Scan() {
-			return errors.New("нет координат")
-		}
-		s := applier.fileReader.Text()
-		trjItem := trjItemFromString(strings.Trim(s, " "))
-		if mon, ok := applier.monomers[int64(trjItem.AtomID)]; ok {
-			newMonomerCoords := applier.calculateNewMonomerCoords(mon.Coords(), trjItem, box)
-			if err := field.MoveMonomer(mon, newMonomerCoords); err != nil {
-				return err
+func (applier *_TrajectoriesApplier) makeStep(numberOfAtoms int) error {
+	for applier.fileReader.Scan() {
+		if strings.Contains(applier.fileReader.Text(), "Coordinates") {
+			for range numberOfAtoms {
+				line := base.ReduceSpacesToSingle(applier.fileReader.Text())
+				line = strings.Trim(line, " ")
+				if err := applier.setNewCoords(line); err != nil {
+					return err
+				}
+				applier.fileReader.Scan()
 			}
+			return nil
 		}
+	}
+	return errors.New("не найдено координат")
+}
+
+func (applier *_TrajectoriesApplier) setNewCoords(line string) error {
+	parts := strings.Split(line, " ")
+	numberStr := parts[len(parts)-4]
+	number, err := strconv.ParseInt(numberStr, 10, 64)
+	if err != nil {
+		return err
+	}
+	xStr := parts[len(parts)-3]
+	x, err := strconv.ParseFloat(xStr, 64)
+	if err != nil {
+		return err
+	}
+	yStr := parts[len(parts)-2]
+	y, err := strconv.ParseFloat(yStr, 64)
+	if err != nil {
+		return err
+	}
+	zStr := parts[len(parts)-1]
+	z, err := strconv.ParseFloat(zStr, 64)
+	if err != nil {
+		return err
+	}
+
+	if mon, ok := applier.monomers[number]; ok {
+		field := applier.globula.GetPolymerByIdx(0).GetUnderlinedField()
+		return field.MoveMonomer(mon, base.Vector3DF{x, y, z})
+	} else {
+		return fmt.Errorf("нет мономера с номером %d", number)
+	}
+}
+
+func (applier *_TrajectoriesApplier) calculateS(timestamp int, file *os.File) error {
+	SMeanCos, STensor, err := cristallinity.AnalyzeToOutside(applier.globula, 3, cristallinity.Atomistic, base.Carbon, 0.05, "s_process.log")
+	if err != nil {
+		return err
+	}
+	if _, err = fmt.Fprintf(file, "%d;%s;%s\n",
+		timestamp,
+		strconv.FormatFloat(SMeanCos, 'f', 3, 64),
+		strconv.FormatFloat(STensor, 'f', 3, 64),
+	); err != nil {
+		return err
 	}
 	return nil
 }
@@ -172,7 +211,7 @@ func (applier *_TrajectoriesApplier) saveTimestep(timestep int) error {
 	if s, err := savers.SaveToLammps(applier.globula); err != nil {
 		return err
 	} else {
-		filename := "timestep_" + applier.trajectoriesFileName + "_" + strconv.Itoa(timestep) + ".dump"
+		filename := "timestamp_" + strconv.Itoa(timestep) + ".dump"
 		file, err := os.Create(filename)
 		if err != nil {
 			return err
@@ -185,8 +224,8 @@ func (applier *_TrajectoriesApplier) saveTimestep(timestep int) error {
 	return nil
 }
 
-func ApplyTrajectories(globula *views.GlobulaView, trajectoryID int, trajectoriesFilename string) error {
-	applier, err := newTrajectoriesApplier(globula, trajectoriesFilename)
+func ApplyTrajectories(globula *views.GlobulaView, trajectoriesFilenames []string) error {
+	applier, err := newTrajectoriesApplier(globula, trajectoriesFilenames)
 	if err != nil {
 		return err
 	}
