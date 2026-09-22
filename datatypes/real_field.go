902cdbb87296ef3ae83bc1411c0a2a73075101a2
Ivan Patrenkov <ivanestver24@gmail.com>
2026-09-22 21:47:44 +0300

    add: rewrite the real field storage

--- datatypes/real_field.go
+++ datatypes/real_field.go
@@ -5,10 +5,130 @@ import (
 	"math"
 	"polymers/base"
 	"polymers/globaldata"
+	"slices"
 )
 
+type _CmpResult int
+
+const (
+	_Less _CmpResult = iota
+	_Exact
+	_Greater
+)
+
+type _Node struct {
+	Coordinate float64
+	Subnodes   []_Node
+	Monomer    *Monomer
+}
+
+type _RealFieldStorage struct {
+	Nodes []_Node
+}
+
+func (realField *_RealFieldStorage) GetMonomer(coords base.Vector3DF) *Monomer {
+	if leftX, _, isExact := realField.find(coords[base.AxisX], realField.Nodes); isExact {
+		subnodes := realField.Nodes[leftX].Subnodes
+		if leftY, _, isExact := realField.find(coords[base.AxisY], subnodes); isExact {
+			subnodes := subnodes[leftY].Subnodes
+			if leftZ, _, isExact := realField.find(coords[base.AxisZ], subnodes); isExact {
+				return subnodes[leftZ].Monomer
+			} else {
+				newMonomer := NewMonomer(coords, base.C)
+				realField.Nodes[leftX].Subnodes[leftY].Subnodes = append(realField.Nodes[leftX].Subnodes[leftY].Subnodes, _Node{
+					Coordinate: coords[base.AxisZ],
+					Monomer:    newMonomer,
+				})
+				slices.SortFunc(realField.Nodes[leftX].Subnodes[leftY].Subnodes, func(left, right _Node) int {
+					if left.Coordinate < right.Coordinate {
+						return -1
+					} else if left.Coordinate > right.Coordinate {
+						return 1
+					} else {
+						return 0
+					}
+				})
+				return newMonomer
+			}
+		} else {
+			newMonomer := NewMonomer(coords, base.C)
+			realField.Nodes[leftY].Subnodes = append(realField.Nodes[leftY].Subnodes, _Node{
+				Coordinate: coords[base.AxisY],
+				Subnodes: []_Node{
+					{
+						Coordinate: coords[base.AxisZ],
+						Monomer:    newMonomer,
+					},
+				},
+			})
+			slices.SortFunc(realField.Nodes[leftY].Subnodes, func(left, right _Node) int {
+				if left.Coordinate < right.Coordinate {
+					return -1
+				} else if left.Coordinate > right.Coordinate {
+					return 1
+				} else {
+					return 0
+				}
+			})
+			return newMonomer
+		}
+	} else {
+		newMonomer := NewMonomer(coords, base.C)
+		realField.Nodes = append(realField.Nodes, _Node{
+			Coordinate: coords[base.AxisX],
+			Subnodes: []_Node{
+				{
+					Coordinate: coords[base.AxisY],
+					Subnodes: []_Node{
+						{
+							Coordinate: coords[base.AxisZ],
+							Monomer:    newMonomer,
+						},
+					},
+				},
+			},
+		})
+		slices.SortFunc(realField.Nodes, func(left, right _Node) int {
+			if left.Coordinate < right.Coordinate {
+				return -1
+			} else if left.Coordinate > right.Coordinate {
+				return 1
+			} else {
+				return 0
+			}
+		})
+		return newMonomer
+	}
+}
+
+func (realField *_RealFieldStorage) find(value float64, nodes []_Node) (left, right int, isExact bool) {
+	left = 0
+	right = len(nodes) - 1
+	isExact = false
+	if len(nodes) == 1 {
+		isExact = base.CompareFloatWithE(value, nodes[left].Coordinate, 0.001)
+		return
+	}
+	for left < right {
+		m := (left + right) / 2
+		middle := nodes[m].Coordinate
+		if base.CompareFloatWithE(value, middle, 0.001) {
+			left = m
+			right = m
+			isExact = true
+			return
+		} else if value < middle {
+			right = m
+		} else {
+			left = m
+		}
+	}
+	return
+}
+
 type RealField struct {
-	monomers map[base.Vector3DF]*Monomer
+	monomers  map[base.Vector3DF]*Monomer
+	monomers2 *_RealFieldStorage
 }
 
 func NewRealField(restrictions [3][2]float64) *RealField {
@@ -19,6 +139,9 @@ func NewRealField(restrictions [3][2]float64) *RealField {
 		{Lower: restrictions[2][0], Higher: restrictions[2][1]},
 	})
 	realField.monomers = make(map[base.Vector3DF]*Monomer)
+	realField.monomers2 = &_RealFieldStorage{
+		Nodes: make([]_Node, 0),
+	}
 	return realField
 }
 
@@ -59,14 +182,15 @@ func (realField *RealField) GetMonomerByCoords(coords base.Vector3DF) *Monomer {
 	if !globaldata.GetGlobalData().SpaceDimention.PointInSpace(&coords) {
 		return nil
 	}
-	roundedCoords := realField.roundCoords(coords)
-	if m, ok := realField.monomers[roundedCoords]; ok {
-		return m
-	} else {
-		m1 := NewMonomer(roundedCoords, base.C)
-		realField.monomers[roundedCoords] = m1
-		return m1
-	}
+	// roundedCoords := realField.roundCoords(coords)
+	// if m, ok := realField.monomers[roundedCoords]; ok {
+	// 	return m
+	// } else {
+	// 	m1 := NewMonomer(roundedCoords, base.C)
+	// 	realField.monomers[roundedCoords] = m1
+	// 	return m1
+	// }
+	return realField.monomers2.GetMonomer(coords)
 }
 
 func (realField *RealField) roundCoords(coords base.Vector3DF) base.Vector3DF {
